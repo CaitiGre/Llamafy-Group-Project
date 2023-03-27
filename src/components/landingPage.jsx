50807aadfc327b2fb805ac9ef60064a9f9f55052
CaitiGre <cgre321@aucklanduni.ac.nz>
2023-03-27 13:44:44 +1300

    updated landing pages

--- src/components/landingPage.jsx
+++ src/components/landingPage.jsx
@@ -2,26 +2,34 @@ import styles from './landingPage.css';
 
 function LandingPage(){
     return(
+        /** A div that contains the main components for the landing page. Contains three separate divs. 
+         * The first is for the top section of the landing page that controls the buttons and the title
+         * The aboutContainer contains the first section of the about page that explains about what Llamafy does
+         * The third div explains why customers should use our product.
+         **/
         <div>
             <div className="landingPageContainer">
-            <h1>LLAMAFY</h1>
-            <h2>IT'S TIME TO BUTTON UP OR SHUT UP</h2>
-            <div className="buttonContainer">
-                <button>LOGIN</button>
-                <button>REGISTER</button> 
+                <h1>LLAMAFY</h1>
+                <h2>IT'S TIME TO BUTTON UP OR SHUT UP</h2>
+                <div className="buttonContainer">
+                    <button>LOGIN</button>
+                    <button>REGISTER</button> 
+                </div>
+            </div>
+            <h2 className="llamaSubheading">No time for drama? Trust in Llama!</h2>
+            <div className="aboutContainer">
+                <p>Have you ever wanted to feel like Cher from clueless minus the incestuous undertones? Well now you can! LLAMAFY was created to ease your worries and allow you to make one less decision in your already overwhelming day. Unlike other applications that can make suggestions surrounding potential wardrobe choices, LLAMAFY employs the use of artificial intelligence to account for your gender, general clothing preferences based on what you already have in your wardrobe, and the temperature of your home location. This way you can rest easy knowing you will not be suggested speedos when it is 10 degrees celcius outside! Amazing!</p>
+                <div className="parallaxContainer">Some form of gif type thing here</div>
+            </div>
+            <div className="aboutContainer2">
+                <div className="gifContainer"> Some form of gif type thing here</div>
+                <p>Simply register with us, answer some basic questions about your current wardrobe and you're done! Worry not, these answers can be altered later and change as your style and current wardrobe change too. We can save you up to 16* hours a week in choosing clothes to wear with the simple click of a button. Why waste time opening and closing the closet trying to decide what to wear? No longer do you have to take the Steve Jobs approach of wearing the same outfit day in and day out simply to have one less decision to make per day. Try LLAMAFY today!
+                    <br></br>
+                    <br></br>
+                *Don't quote us on this. We're just enthusiastic! Bordering on unhinged! Just look at all the exclamation marks!</p>
             </div>
         </div>
-        <div className="aboutContainer">
-            <h2>No time for drama? Trust in Llama!</h2>
-            <p>LLAMAFY was created to ease your worries and allow you to make one less decision in your already overwhelming day. Unlike other applications that can make suggestions surrounding potential wardrobe choices, LLAMAFY employs the use of artificial intelligence to account for your temperature, gender and general clothing preferences based on what you already have in your wardrobe.</p>
-            <div className="parallaxImage"></div>
-        </div>
-        <div className="aboutContainer2">
-            <p>Simply register with us, answer some basic questions about your current wardrobe and you're done! We can save you up to 16* hours a week in choosing clothes to wear with the simple click of a button.</p>
-            <p>*Don't quote us on this. We're just enthusiastic</p>
-        </div>
-        </div>
-        
+        /**The bottom section is split into two separate sections */
         
     )
 }
