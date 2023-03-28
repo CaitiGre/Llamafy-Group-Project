7b38a73aa161324ed3333115149332f8df40b237
CassandraPham <cpha723@aucklanduni.ac.nz>
2023-03-29 01:25:35 +1300

    Login design temp done

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,10 @@ import LoginPage from './components/LoginPage';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
       <React.Fragment>
         <Routes>
-          <Route path="/" element={<LoginPage />} />
+          {/* <Route path="/" element={<LandingPage />} /> */}
+          <Route path="/login" element={<LoginPage />} />
 
         </Routes>
       </React.Fragment>
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,8 +1,51 @@
+import './LoginPage.module.css';
+import CloudBackground from '../assets/full-opacity-cloud.jpg';
+import { Parallax } from 'react-parallax';
+import { Box } from "@mui/material";
+
 function LoginPage() {
     return (
+
         <div>
-            <p>Landing Page</p>
+            <Parallax bgImage={CloudBackground} strength={550} className="backgroundImage">
+                <div style={{ width: "cover", height: "100vh", margin: "auto" }} >
+
+                    <h1>LOGIN</h1>
+
+                    <div>
+                        <form>
+                            <Box display="flex" flexDirection="column" alignItems="center">
+                                <label htmlFor="email">Username/ Email</label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    // value={}
+                                    // onChange={}
+                                    placeholder="llama@lavenderllama.co.nz"
+                                />
+                            </Box>
+
+                            <Box display="flex" flexDirection="column" alignItems="center">
+                                <label htmlFor="password">Password</label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                // value={}
+                                // onChange={}
+                                />
+                            </Box>
+
+                            <button id="submit-button" type="submit">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </Parallax>
         </div>
+
+
+
     )
 }
 
