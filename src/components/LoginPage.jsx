d59e7ddc14fa347e6a6831cb77853d2757630c41
CassandraPham <cpha723@aucklanduni.ac.nz>
2023-04-11 22:48:58 +1200

    Updated input fields using mui props

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,7 +1,7 @@
 import './LoginPage.module.css';
 import CloudBackground from '../assets/full-opacity-cloud.jpg';
 import { Parallax } from 'react-parallax';
-import { Box } from "@mui/material";
+import { Box, Input, InputLabel, TextField } from "@mui/material";
 import { useState } from "react";
 
 function LoginPage() {
@@ -12,11 +12,33 @@ function LoginPage() {
     });
 
     const handleChange = (event) => {
-        setData({ ...data, [event.target.name]: event.target.value });
+        setData({ [event.target.name]: event.target.value });
     };
 
     console.log(data);
 
+
+    const inputData = [
+        {
+            displayName: "Username/ Email",
+            type: "email",
+            name: "email",
+            id: "email",
+            value: data.email,
+            onChange: handleChange,
+            placeHolder: "llama@lavenderllama.co.nz"
+        },
+
+        {
+            displayName: "Password",
+            type: "password",
+            name: "password",
+            id: "password",
+            value: data.password,
+            onChange: handleChange,
+            placeHolder: ""
+        }
+    ]
 
     return (
 
@@ -28,30 +50,38 @@ function LoginPage() {
 
                     <div>
                         <form>
-                            <Box display="flex" flexDirection="column" alignItems="center">
-                                <label htmlFor="email">Username/ Email</label>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    id="email"
-                                    value={data.email}
-                                    onChange={handleChange}
-                                    placeholder="llama@lavenderllama.co.nz"
-                                />
-                            </Box>
 
-                            <Box display="flex" flexDirection="column" alignItems="center">
-                                <label htmlFor="password">Password</label>
-                                <input
-                                    type="password"
-                                    name="password"
-                                    id="password"
-                                    value={data.password}
-                                    onChange={handleChange}
-                                />
-                            </Box>
+                            {inputData.map((item) => (
+                                <Box display="flex" flexDirection="column" alignItems="center">
+                                    <InputLabel
+                                        sx={{
+                                            marginBottom: "5px"
+                                        }}>
+                                        {item.displayName}
+                                    </InputLabel>
 
-                            <button id="submit-button" type="submit">Submit</button>
+                                    <Input
+                                        sx={{
+                                            borderRadius: "20px",
+                                            boxShadow: "2px 2px 5px rgba(255, 255, 255, 0.8)",
+                                            height: "40px",
+                                            width: "40%",
+                                            marginBottom: "35px",
+                                            backgroundColor: "white",
+                                        }}
+                                        type={item.type}
+                                        name={item.name}
+                                        id={item.id}
+                                        value={item.value}
+                                        onChange={handleChange}
+                                        inputProps={{ style: { textAlign: "center" } }}
+                                        disableUnderline={true}
+                                        placeholder={item.placeHolder}
+                                    />
+                                </Box>
+                            ))}
+
+                            <button id="submit-button" type="submit" className='login-button'>Submit</button>
                             {/* To add functionality to button: send data to database? */}
                         </form>
                     </div>
