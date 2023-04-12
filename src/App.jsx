6f4479f00f82672090cdbb7d4e49ecb7d83da65d
CassandraPham <cpha723@aucklanduni.ac.nz>
2023-04-13 00:14:37 +1200

    Added SettingsPage to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginPage from './components/LoginPage';
+import SettingsPage from './components/SettingsPage';
+import SettingsSubPage from './components/Profile';
 
 function App() {
   return (
@@ -9,7 +11,9 @@ function App() {
       <React.Fragment>
         <Routes>
           {/* <Route path="/" element={<LandingPage />} /> */}
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="login" element={<LoginPage />} />
+
+          <Route path="settings" element={<SettingsPage />} />
 
         </Routes>
       </React.Fragment>
