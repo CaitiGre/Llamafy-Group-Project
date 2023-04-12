a544ff59d39fcc480eb4864e5c63d94aed0568ca
kralkapoor <kkap897@aucklanduni.ac.nz>
2023-04-12 18:18:40 +1200

    imported styles to login page and isolated styling

---
+++ src/components/OutfitOfTheDay/OutfitOfTheDay.jsx
@@ -0,0 +1,17 @@
+// import mui from 'mui';
+import { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import React from 'react'
+
+const OutfitOfTheDay = ( {} ) => {
+
+    return (
+        <React.Fragment>
+            hey
+
+        </React.Fragment>
+    );
+
+}
+
+export default OutfitOfTheDay;
