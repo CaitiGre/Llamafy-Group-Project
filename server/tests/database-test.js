6f6ad73cec6a5fb2a371b2011cbd2b32ab103ad4
Yash Rraj Sood <yashsood978@gmail.com>
2023-04-16 11:38:55 +1200

    testing db connection

---
+++ server/tests/database-test.js
@@ -0,0 +1,13 @@
+const pool = require('../database/database');
+
+async function testConnection() {
+  try {
+    const connection = await pool.getConnection();
+    console.log('Connected to database successfully!');
+    connection.release();
+  } catch (error) {
+    console.error('Failed to connect to database:', error);
+  }
+}
+
+testConnection();
