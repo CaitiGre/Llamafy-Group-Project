e9283995e28fa5b18e09e012b48f5d1182c88849
kralkapoor <kkap897@aucklanduni.ac.nz>
2023-04-17 13:30:28 +1200

    ootd styling and homepage wind direction

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
     // weather api key - to put in to .env later
     const weatherKey = "e9c2f39101b44170a9753323231504";
     // need an axios DB call for user's city. setting to auckland for now
-    const [city, setCity] = useState("Paris");
+    const [city, setCity] = useState("Auckland");
 
     const call = `https://api.weatherapi.com/v1/current.json?key=${weatherKey}&q=${city}&aqi=no` //AQI = air quality data
 
@@ -21,6 +21,7 @@ const HomePage = () => {
         tempC : '',
         humidity : '',
         windKph : '',
+        windDir : '',
         iconUrl : '',
     });
 
@@ -33,6 +34,7 @@ const HomePage = () => {
         tempC : `${res.data.current.temp_c} C`,
         humidity : `${res.data.current.humidity}%`,
         windKph : `${res.data.current.wind_kph} km/h`,
+        windDir : res.data.current.wind_dir,
         iconUrl : res.data.current.condition.icon});})
     .catch(err => {console.log(err);
                     setWeatherValues(undefined)});
@@ -59,7 +61,7 @@ const HomePage = () => {
             <div className={styles.title}> 
             <img src={weatherValues.iconUrl} alt="Weather symbol" /> <br />
             Hey username. {weatherValues.condition} in {weatherValues.location}.
-            <p>{weatherValues.tempC} right now with {weatherValues.humidity} humidity. Windiness rated at {weatherValues.windKph}. <small>Powered by <a href="https://www.weatherapi.com/" title="Weather API">WeatherAPI.com</a></small></p>
+            <p>{weatherValues.tempC} right now with {weatherValues.humidity} humidity. Windiness rated at {weatherValues.windKph} {weatherValues.windDir}. <small>Powered by <a href="https://www.weatherapi.com/" title="Weather API">WeatherAPI.com</a></small></p>
             </div>
         : 
             <div className={styles.title}> Hello username! 
