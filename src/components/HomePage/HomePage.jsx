65ec281f84242b55d27c6d3e49b522dfa59a8672
kralkapoor <kkap897@aucklanduni.ac.nz>
2023-04-17 12:47:39 +1200

    added weather icon to homepage from weatherAPI url

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
     // weather api key - to put in to .env later
     const weatherKey = "e9c2f39101b44170a9753323231504";
     // need an axios DB call for user's city. setting to auckland for now
-    const [city, setCity] = useState("Malta");
+    const [city, setCity] = useState("Paris");
 
     const call = `https://api.weatherapi.com/v1/current.json?key=${weatherKey}&q=${city}&aqi=no` //AQI = air quality data
 
@@ -21,15 +21,19 @@ const HomePage = () => {
         tempC : '',
         humidity : '',
         windKph : '',
+        iconUrl : '',
     });
 
     useEffect(() => {axios.get(call)
-    .then(res => setWeatherValues({...weatherValues, 
+    .then(res => {
+        console.log(res.data);
+        setWeatherValues({...weatherValues, 
         currTime : res.data.location.localtime,
         condition : res.data.current.condition.text,
         tempC : `${res.data.current.temp_c} C`,
         humidity : `${res.data.current.humidity}%`,
-        windKph : `${res.data.current.wind_kph} km/h`}),)
+        windKph : `${res.data.current.wind_kph} km/h`,
+        iconUrl : res.data.current.condition.icon});})
     .catch(err => {console.log(err);
                     setWeatherValues(undefined)});
     }, []);
@@ -46,18 +50,21 @@ const HomePage = () => {
     ];
 
     const [pastOutfits, setPastOutfits] = useState(tempArr);
-
     
     return <div className={styles.homePage}>
 
         
         {weatherValues 
         ?   
-            <div className={styles.title}> Hey username. {weatherValues.condition} in {weatherValues.location}.
+            <div className={styles.title}> 
+            <img src={weatherValues.iconUrl} alt="Weather symbol" /> <br />
+            Hey username. {weatherValues.condition} in {weatherValues.location}.
             <p>{weatherValues.tempC} right now with {weatherValues.humidity} humidity. Windiness rated at {weatherValues.windKph}. <small>Powered by <a href="https://www.weatherapi.com/" title="Weather API">WeatherAPI.com</a></small></p>
             </div>
         : 
-            <div className={styles.title}> Hello llama! </div>
+            <div className={styles.title}> Hello username! 
+            <p>Unable to fetch weather details at the moment. Try again soon.</p>
+            </div>
         }
         
 
