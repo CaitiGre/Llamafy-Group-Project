b1d9170ca374035caa9ae35919758ef6048c8484
kinzi ceolin <kceo161@aucklanduni.ac.nz>
2023-04-17 20:57:23 +1200

    Add Clothe Configuration elements to SubSelectionModal

--- src/components/SubSelectionModal/SubSelectionModal.jsx
+++ src/components/SubSelectionModal/SubSelectionModal.jsx
@@ -70,46 +70,139 @@ function SubSelectionModal({ itemsToShow }) {
         <Modal
           open={showModal}
           onClose={onCloseModal}
-          sx={{ overflowY: "scroll" }}
+          sx={{
+            justifyContent: "center",
+            display: "flex",
+            alignItems: "center",
+          }}
         >
           <Card
             sx={{
               borderRadius: "16px",
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
+              height: "max-content",
               outline: "none",
               outline: 0,
-              height: "fit-content",
+              maxHeight: "90vh",
+              minWidth: "50vw",
               margin: "auto",
               padding: "40px",
+              overflowY: "scroll",
             }}
           >
-            <Button
-              onClick={onCloseModal}
-              sx={{ position: "absolute", top: 8, right: 2, padding: "5px" }}
-            >
-              <img src={close} alt="close button" width="20px" />
-            </Button>
-            <Typography variant="h4">{selectedItem.name}</Typography>
-            <Grid>
-              <Typography>Colour</Typography>
-              <Box>
-                <SketchPicker
-                  width={200}
-                  height={200}
-                  color={color}
-                  onChange={handleColorChange}
-                />
-              </Box>
-              <Typography>Length</Typography>
-              <Box></Box>
-              <Typography>Style</Typography>
-              <Box></Box>
-              <Typography>Pattern</Typography>
-              <Box></Box>
-            </Grid>
+            <Box sx={{ position: "relative" }}>
+              <Button
+                onClick={onCloseModal}
+                sx={{ position: "absolute", top: 8, right: 2, padding: "5px" }}
+              >
+                <img src={close} alt="close button" width="20px" />
+              </Button>
+              <Typography variant="h4" sx={{ textTransform: "uppercase" }}>
+                {selectedItem.name}
+              </Typography>
+              <Grid>
+                <Box>
+                  <Typography
+                    variant="h5"
+                    sx={{ paddingTop: "20px", paddingBottom: "20px" }}
+                  >
+                    Colour
+                  </Typography>
+
+                  <SketchPicker
+                    width={300}
+                    height={300}
+                    color={color}
+                    onChange={handleColorChange}
+                  />
+                </Box>
+                <Box>
+                  <Typography
+                    variant="h5"
+                    sx={{ paddingTop: "20px", paddingBottom: "20px" }}
+                  >
+                    Length
+                  </Typography>
+                  <Grid
+                    container
+                    spacing={{ xs: 4, md: 4 }}
+                    columns={4}
+                    sx={{
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Grid item>
+                      <Card sx={{ padding: "20px" }}>short</Card>
+                    </Grid>
+                    <Grid item>
+                      <Card sx={{ padding: "20px" }}>1/2</Card>
+                    </Grid>
+                    <Grid item>
+                      <Card sx={{ padding: "20px" }}>3/4</Card>
+                    </Grid>
+                    <Grid item>
+                      <Card sx={{ padding: "20px" }}>full</Card>
+                    </Grid>
+                  </Grid>
+                </Box>
+                <Box>
+                  <Typography
+                    variant="h5"
+                    sx={{ paddingTop: "20px", paddingBottom: "20px" }}
+                  >
+                    Style
+                  </Typography>
+                  <Grid
+                    container
+                    spacing={{ xs: 4, md: 4 }}
+                    columns={3}
+                    sx={{
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Grid item>
+                      <Card sx={{ padding: "20px" }}>flared</Card>
+                    </Grid>
+                    <Grid item>
+                      <Card sx={{ padding: "20px" }}>loose</Card>
+                    </Grid>
+                    <Grid item>
+                      <Card sx={{ padding: "20px" }}>tight</Card>
+                    </Grid>
+                  </Grid>
+                </Box>
+
+                <Box>
+                  <Typography
+                    variant="h5"
+                    sx={{ paddingTop: "20px", paddingBottom: "20px" }}
+                  >
+                    Pattern
+                  </Typography>
+                  <Grid
+                    container
+                    spacing={{ xs: 4, md: 4 }}
+                    columns={3}
+                    sx={{
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Grid item>
+                      <Card sx={{ padding: "20px" }}>floral</Card>
+                    </Grid>
+                    <Grid item>
+                      <Card sx={{ padding: "20px" }}>stripes</Card>
+                    </Grid>
+                    <Grid item>
+                      <Card sx={{ padding: "20px" }}>polka dots</Card>
+                    </Grid>
+                  </Grid>
+                </Box>
+                <Box sx={{ position: "absolute" }}>
+                  <Button>Add</Button>
+                  <Button onClick={onCloseModal}>Cancel</Button>
+                </Box>
+              </Grid>
+            </Box>
           </Card>
         </Modal>
       )}
