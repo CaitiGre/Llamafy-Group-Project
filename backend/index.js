7cb05eac83ecd12538f655ce0fe417d2d438d3ca
Yash Rraj Sood <yashsood978@gmail.com>
2023-04-18 23:48:00 +1200

    minor update to index.js
    added CORS support.

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,22 @@ const registrationPageRouter = require('./routes/RegistrationPage');
 const app = express();
 const port = process.env.PORT || 3006;
 
+// Add CORS middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/registration', registrationPageRouter);
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
-  });
+  res.send('Hello, world!');
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}.`);
