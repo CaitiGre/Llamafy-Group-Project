5d4786fd1fbba2cc2df680257b71f654c111b222
kinzi ceolin <kceo161@aucklanduni.ac.nz>
2023-04-19 20:25:41 +1200

    Add backend routing for WardrobeSelection page

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const registrationPageRouter = require('./routes/RegistrationPage');
+const express = require("express");
+const bodyParser = require("body-parser");
+const registrationPageRouter = require("./routes/RegistrationPage");
+const wardrobePageRouter = require("/routes/WardrobePage");
 
 const app = express();
 const port = process.env.PORT || 3006;
@@ -8,11 +9,12 @@ const port = process.env.PORT || 3006;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/registration', registrationPageRouter);
+app.use("/registration", registrationPageRouter);
+app.use("/wardrobeSelection", wardrobePageRouter);
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
-  });
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}.`);
