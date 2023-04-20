562527a23fc734f84e04e9020209d576bff2e1f4
kralkapoor <kkap897@aucklanduni.ac.nz>
2023-04-20 17:10:51 +1200

    llama placeholder pic ootd

--- frontend/src/components/OutfitOfTheDay/OutfitOfTheDay.jsx
+++ frontend/src/components/OutfitOfTheDay/OutfitOfTheDay.jsx
@@ -6,8 +6,8 @@ const OutfitOfTheDay = () => {
 
     const temp = [
         {id : 1, img : 'images/good-bad-ugly.jpeg', desc : "A cozy poncho and fleece vest topped with a dapper hat."},
-        {id : 2, img : 'images/dirty_harry.jpeg', desc: "Grey tweed jacket over a red knitted vest with a dashing tie. Formal justice wear."},
-        {id : 3, img : 'images/unforgiven.jpg', desc: "Thick warm coat and tidy brown polo. Relatively dishevelled."}
+        {id : 2, img : 'images/triple.png', desc: "Purple wool"},
+        {id : 3, img : 'images/dirty_harry.jpeg', desc: "Grey tweed jacket over a red knitted vest with a dashing tie. Formal justice wear."},
     ];
 
     const [recommendations, setRecommendations] = useState(temp);
