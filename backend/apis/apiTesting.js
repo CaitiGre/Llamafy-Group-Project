d8910c138774bcc1e25232778d765a67d15f5b7d
kralkapoor <kkap897@aucklanduni.ac.nz>
2023-04-26 10:45:04 +1200

    gpt prompt development

--- backend/apis/apiTesting.js
+++ backend/apis/apiTesting.js
@@ -14,43 +14,78 @@ const openAi = new OpenAIApi(configuration);
 
 let prompt = 
 
-`Given the following JSON, suggest an outfit to wear today given that the temperature outside is 0 degrees celsius.
+`Given the following JSON of clothes, suggest three outfits to wear today given that the temperature outside is 20 degrees celsius and partly cloudy.
 {
     tops : [
         {id : 1, description : "plain", colour : "white", subCategory : "longsleeve"},
-        {id : 400, description : "extremely summery linen", colour : "blue", subCategory : "dressShirt"}
+        {id : 400, description : "extremely summery linen", colour : "blue", subCategory : "dressShirt"},
+        {id : 69, description : "overcoat made of pure orangatang", colour: "orange", subCategory : "overcoat"},
+        {id : 47, description : "white dress shirt", colour : "red", subCategory : "dressShirt"},
+        {id : 48, description : "wifebeater", colour : "white", subCategory : "singlet"}
     ],
     bottoms: [
         {id : 2, description : "riddled with holes", colour : "rainbow", subCategory : "shorts"},
-        {id : 3, description : "adidas trackpants", colour : "black", subCategory : "trackpants"}
+        {id : 3, description : "adidas trackpants", colour : "black", subCategory : "trackpants"},
+        {id : 0, description : "extremely mini", colour : "pink", subCategory : "skirts"},
+        {id : 11, description : "fleece leggings", colour : "green", subCategory : "activewear"},
+        {id : 42, description : "long fluffy skirt", colour : "purple", subCategory : "skirts"}
     ],
     one_piece:
     [
         {id : 100, description : "coal mining overalls", colour : "coal", subCategory : "overalls"},
-        {id : 101, description : "very short and flirty", colour : "yellow", subCategory : "dress"}
+        {id : 101, description : "very short and flirty", colour : "yellow", subCategory : "dress"},
+        {id : 102, description : "playsuit", colour : "blue", subCategory : "dress"},
+        {id : 103, description : "HAZMAT suit from Chernobyl", colour : "white", subCategory : "overalls"},
+        {id : 104, description : "mummy wraps", colour : "white", subCategory : "funeralAttire"}
     ],
     shoes:
     [
         {id : 9000, description : "fancy italian leather", colour : "tan", subCategory : "dress"},
-        {id : 1000, description : "standard doc marten's boots", colour : "brown", subCategory : "boots"}
+        {id : 9001, description : "standard doc marten's boots", colour : "brown", subCategory : "boots"},
+        {id : 9002, description : "HAZMAT boots", colour : "white", subCategory : "boots"},
+        {id : 9003, description : "extremely fluffy", colour : "pink", subCategory : "slippers"},
+        {id : 9004, description : "sandals", colour : "lavender", subCategory : "sandals"}
     ],
 }
-Respond in the below format only, substituting % with the values. Do not provide a value for a category if it is covered by another. In the "dalle" property, provide a prompt for the DALL-E model to generate a realistic photograph of the outfit.
+Respond in the below format only, substituting % with the values. Do not provide a value for a category if it is covered by another. In the "dalle" property, provide a comprehensive prompt to give to the DALL-E model. Focus on providing detail on colour.
 {
-  "top": [
-      {"id": "%", "description": "%", "colour": "%", "subCategory": "%"}
-  ],
-  "bottom": [
-      {"id": "%", "description": "%", "colour": "%", "subCategory": "%"}
-  ],
-  "one_piece": [
-      {"id": "%", "description": "%", "colour": "%", "subCategory": "%"}
-  ],
-  "shoes": [
-      {"id": "%", "description": "%", "colour": "%", "subCategory": "%"}
-  ],
-  "dalle": "%"
+  "recommendation1" : {
+    "top": [
+        {"id": "%", "description": "%", "colour": "%", "subCategory": "%"}
+    ],
+    "bottom": [
+        {"id": "%", "description": "%", "colour": "%", "subCategory": "%"}
+    ],
+    "onePiece": [
+        {"id": "%", "description": "%", "colour": "%", "subCategory": "%"}
+    ],
+    "shoes": [
+        {"id": "%", "description": "%", "colour": "%", "subCategory": "%"}
+    ],
+    "dalle": "A full-body lookbook style photograph of a male model wearing %"
+  },
+  "recommendation2" : {"top" : "%..."},
 }`;
+
+
+const format = {
+  "recommendation1" : {
+    "top": [
+        {"id": "%", "description": "%", "colour": "%", "subCategory": "%"}
+    ],
+    "bottom": [
+        {"id": "%", "description": "%", "colour": "%", "subCategory": "%"}
+    ],
+    "onePiece": [
+        {"id": "%", "description": "%", "colour": "%", "subCategory": "%"}
+    ],
+    "shoes": [
+        {"id": "%", "description": "%", "colour": "%", "subCategory": "%"}
+    ],
+    "dalle": "%"
+  },
+  "recommendation2" : {"top" : "%..."},
+}
 
 async function generateAnswer() {
     try {
@@ -65,23 +100,29 @@ async function generateAnswer() {
       });
 
       // console.log(response.data);
-
       const responseText = response.data.choices[0].text;
-      // responseText.trim();
-      // const tokensUsed = response.data.usage.total_tokens;
-
       console.log(responseText);
-    //   console.log(tokensUsed);
+      // const tokensUsed = response.data.usage.total_tokens;
+      //   console.log(tokensUsed);
 
     try {
     const toJson = JSON.parse(responseText);
-    const dallePrompt = toJson.dalle;
-    console.log(dallePrompt);
-    imgGen(dallePrompt);
+
+    const dallePrompt1 = toJson.recommendation1.dalle;
+    const dallePrompt2 = toJson.recommendation2.dalle;
+    const dallePrompt3 = toJson.recommendation3.dalle;
+
+    console.log(dallePrompt1);
+    console.log(dallePrompt2);
+    console.log(dallePrompt3);
+
+    imgGen(dallePrompt1);
+    imgGen(dallePrompt2);
+    imgGen(dallePrompt3);
+
     } catch (dalleErr) {
       console.log(dalleErr);
     }
-
     } catch (error) {
       console.log(error);
     }
