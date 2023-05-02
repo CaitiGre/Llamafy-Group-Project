d58351bdb5b7cb90de3414e0acb1f1ed58c953db
kinzi ceolin <kceo161@aucklanduni.ac.nz>
2023-05-03 11:28:32 +1200

    Fix handleDelete

--- frontend/src/components/LoginPage/LoginPage.jsx
+++ frontend/src/components/LoginPage/LoginPage.jsx
@@ -1,121 +1,130 @@
-import './LoginPage.module.css';
-import { Box, Input, InputLabel, Typography } from "@mui/material";
+import "./LoginPage.module.css";
+import { Box, Input, InputLabel } from "@mui/material";
 import { useState } from "react";
-import styles from './LoginPage.module.css';
-import Heading from '../Heading/Heading';
+import styles from "./LoginPage.module.css";
+import Heading from "../Heading/Heading";
 
-function LoginPage() { 
+function LoginPage() {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [data, setData] = useState({
-        email: '',
-        password: ''
-    });
+  const handleChange = (event) => {
+    setData((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
 
-    const handleChange = (event) => {
-        setData(prevState => ({ ...prevState, [event.target.name]: event.target.value }));
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-    const handleSubmit = async (event) => {
-
-        event.preventDefault();
-      
-
-        try {
-            const response = await fetch('http://localhost:3006/auth/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    username: data.email,
-                    password: data.password,
-                }),
-                credentials: 'include'
-            });
-
-            if (response.ok) {
-                const result = await response.json();
-                alert(result.message);
-                window.location.href = '/wardrobe';
-            } else {
-                const error = await response.json();
-                console.error(error);
-                alert("Invalid username or password. Please try again.");
-            }
-        } catch (err) {
-            console.error(err);
-            alert("An error occurred while logging in. Please try again.");
-        }
-    };
-
-
-    const inputData = [
-        {
-            displayName: "Username/ Email",
-            type: "email",
-            name: "email",
-            id: "email",
-            value: data.email,
-            onChange: handleChange,
-            placeHolder: "llama@lavenderllama.co.nz"
+    try {
+      const response = await fetch("http://localhost:3006/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
+        body: JSON.stringify({
+          username: data.email,
+          password: data.password,
+        }),
+        credentials: "include",
+      });
 
-        {
-            displayName: "Password",
-            type: "password",
-            name: "password",
-            id: "password",
-            value: data.password,
-            onChange: handleChange,
-            placeHolder: ""
-        }
-    ]
+      if (response.ok) {
+        const result = await response.json();
+        alert(result.message);
+        window.location.href = "/wardrobe";
+      } else {
+        const error = await response.json();
+        console.error(error);
+        alert("Invalid username or password. Please try again.");
+      }
+    } catch (err) {
+      console.error(err);
+      alert("An error occurred while logging in. Please try again.");
+    }
+  };
 
-    return (
+  const inputData = [
+    {
+      displayName: "Username/ Email",
+      type: "email",
+      name: "email",
+      id: "email",
+      value: data.email,
+      onChange: handleChange,
+      placeHolder: "llama@lavenderllama.co.nz",
+    },
 
-        <div>
-            <Heading title="Login"/>
+    {
+      displayName: "Password",
+      type: "password",
+      name: "password",
+      id: "password",
+      value: data.password,
+      onChange: handleChange,
+      placeHolder: "",
+    },
+  ];
 
-            <div className={styles.formContainer}>
-                <form onSubmit={handleSubmit}>
+  return (
+    <div>
+      <Heading title="Login" />
 
-                    {inputData.map((item) => (
-                        <Box display="flex" flexDirection="column" alignItems="center" key={item.id}>
-                            <InputLabel
-                                sx={{
-                                    marginBottom: "5px",
-                                    color: "#eee"
-                                }}>
-                                {item.displayName}
-                            </InputLabel>
+      <div className={styles.formContainer}>
+        <form onSubmit={handleSubmit}>
+          {inputData.map((item) => (
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              key={item.id}
+            >
+              <InputLabel
+                sx={{
+                  marginBottom: "5px",
+                  color: "#eee",
+                }}
+              >
+                {item.displayName}
+              </InputLabel>
 
-                            <Input required
-                                sx={{
-                                    borderRadius: "20px",
-                                    boxShadow: "2px 2px 5px rgba(255, 255, 255, 0.8)",
-                                    height: "40px",
-                                    width: "85%",
-                                    marginBottom: "35px",
-                                    backgroundColor: "white",
-                                }}
-                                type={item.type}
-                                name={item.name}
-                                id={item.id}
-                                value={item.value}
-                                onChange={handleChange}
-                                inputProps={{ style: { textAlign: "center" } }}
-                                disableUnderline={true}
-                                placeholder={item.placeHolder}
-                            />
-                        </Box>
-                    ))}
+              <Input
+                required
+                sx={{
+                  borderRadius: "20px",
+                  boxShadow: "2px 2px 5px rgba(255, 255, 255, 0.8)",
+                  height: "40px",
+                  width: "85%",
+                  marginBottom: "35px",
+                  backgroundColor: "white",
+                }}
+                type={item.type}
+                name={item.name}
+                id={item.id}
+                value={item.value}
+                onChange={handleChange}
+                inputProps={{ style: { textAlign: "center" } }}
+                disableUnderline={true}
+                placeholder={item.placeHolder}
+              />
+            </Box>
+          ))}
 
-                    <button id="submit-button" type="submit" className={styles.submitButton}>Submit</button>
-
-                </form>
-            </div>
-        </div>
-    )
+          <button
+            id="submit-button"
+            type="submit"
+            className={styles.submitButton}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
