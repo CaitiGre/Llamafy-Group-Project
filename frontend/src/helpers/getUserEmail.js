14049a1d5ee952e08987c32aad3930bbeb48189e
kinzi ceolin <kceo161@aucklanduni.ac.nz>
2023-05-02 21:20:17 +1200

    Add user email to json object

--- frontend/src/helpers/getUserEmail.js
+++ frontend/src/helpers/getUserEmail.js
@@ -1,11 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-async function getUserEmail() {
+export async function getUserEmail() {
   try {
     const response = await axios.get(`/auth/getUserEmail/`);
+    console.log(response.data.user_email);
     return response.data.user_email;
   } catch (error) {
-    console.error('Error getting user email:', error);
+    console.error("Error getting user email:", error);
     return null;
   }
 }
