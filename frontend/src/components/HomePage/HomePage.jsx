c8c2959ac399e967bf5d3eadc1f7b516486a91bc
kralkapoor <kkap897@aucklanduni.ac.nz>
2023-05-02 23:11:07 +1200

    moved weather widget to OOTD

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -9,15 +9,8 @@ const HomePage = () => {
 
     // init states
     let tempArr = [];
-    const [username, setUsername] = useState('UsernameFromCookies');
-    const [weatherValues, setWeatherValues] = useState([]);
     const [fileNames, setFileNames] = useState([]);
     const [pastOutfits, setPastOutfits] = useState([]);
-   
-    // get weather data from weatherAPI proxy
-    useEffect(() => {axios.get('http://localhost:3006/weather/data')
-    .then(res => setWeatherValues(res.data))
-    },[]);
 
     // loop through AI generated images for homepage cards
     // TODO: only loop through the past outfits for the current user
@@ -31,22 +24,6 @@ const HomePage = () => {
     }, [])
     
     return <div className={styles.homePage}>
-
-        {/* If there is an error getting weather values, greeet the user and inform them that the api is not working*/}
-        {weatherValues 
-        ?   
-            <div className={styles.title}> 
-            <img src={weatherValues.iconUrl} alt="Weather icon based on the weather today"/> <br />
-            Hey {username}. {weatherValues.condition} in {weatherValues.location}.
-            <p>{weatherValues.tempC} right now with {weatherValues.humidity} humidity. Windspeed at {weatherValues.windKph}. <small>Powered by <a href="https://www.weatherapi.com/" title="Weather API">WeatherAPI.com</a></small></p>
-            </div>
-        : 
-            <div className={styles.title}> Hello {username}! 
-            <p>Unable to fetch weather details at the moment. Try again soon.</p>
-            </div>
-        }
-        
-        <br />
 
         <SubHeading subtitle="PAST OUTFITS"/>
 
