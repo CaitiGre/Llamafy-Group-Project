13f96855ca7eef658a8ffb3764bb1e318ba238d6
CaitiGre <cgre321@aucklanduni.ac.nz>
2023-05-03 16:03:15 +1200

    updates to login css and registration layout

--- frontend/src/components/RegistrationPage/GenderButtons.jsx
+++ frontend/src/components/RegistrationPage/GenderButtons.jsx
@@ -1,35 +1,61 @@
-import { Box } from "@mui/material";
-import style from '../RegistrationPage/Registration.module.css';
+import { Box, InputLabel, Button } from "@mui/material";
+import style from "../RegistrationPage/Registration.module.css";
 
 export default function GenderButtons({ data, handleButtonClick }) {
-    return (
-        <>
-            <Box display="flex" flexDirection="column" alignItems="center">
-                <label htmlFor="gender" className={style["register-label"]}>Style Preference</label>
-                <div className={style["gender-div"]}>
-                    <button
-                        type="button"
-                        className={`${style['gender-button']} ${data.gender === 'male' ? style['selected'] : ''}`}
-                        onClick={() => handleButtonClick('male')}
-                    >
-                        Male
-                    </button>
-                    <button
-                        type="button"
-                        className={`${style['gender-button']} ${data.gender === 'female' ? style['selected'] : ''}`}
-                        onClick={() => handleButtonClick('female')}
-                    >
-                        Female
-                    </button>
-                    <button
-                        type="button"
-                        className={`${style['gender-button']} ${data.gender === 'other' ? style['selected'] : ''}`}
-                        onClick={() => handleButtonClick('other')}
-                    >
-                        All
-                    </button>
-                </div>
-            </Box>
-        </>
-    );
-};
+  return (
+    <>
+      <Box
+        className={style.GenderButtonBox}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+      >
+        <InputLabel
+          htmlFor="gender"
+          className={style.registerLabel}
+          style={{
+            width: "70vh",
+            display: "flex",
+            justifyContent: "space-around",
+            paddingBottom: "5px",
+            textTransform: "uppercase",
+            fontFamily:
+              "'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif",
+            color: "#fefefe",
+          }}
+        >
+          Style Preference
+        </InputLabel>
+        <div className={style["gender-div"]}>
+          <Button
+            type="button"
+            className={`${style["gender-button"]} ${
+              data.gender === "male" ? style["selected"] : ""
+            }`}
+            onClick={() => handleButtonClick("male")}
+          >
+            Male
+          </Button>
+          <Button
+            type="button"
+            className={`${style["gender-button"]} ${
+              data.gender === "female" ? style["selected"] : ""
+            }`}
+            onClick={() => handleButtonClick("female")}
+          >
+            Female
+          </Button>
+          <Button
+            type="button"
+            className={`${style["gender-button"]} ${
+              data.gender === "other" ? style["selected"] : ""
+            }`}
+            onClick={() => handleButtonClick("other")}
+          >
+            All
+          </Button>
+        </div>
+      </Box>
+    </>
+  );
+}
--- frontend/src/components/RegistrationPage/LocationBox.jsx
+++ frontend/src/components/RegistrationPage/LocationBox.jsx
@@ -1,35 +1,56 @@
-import { Box } from "@mui/material";
-import style from '../RegistrationPage/Registration.module.css';
+import { Box, InputLabel, MenuItem, Select } from "@mui/material";
+import style from "../RegistrationPage/Registration.module.css";
 
 export default function LocationBox({ data, handleChange }) {
+  const locations = [
+    "Auckland",
+    "Wellington",
+    "Christchurch",
+    "Dunedin",
+    "Invercargill",
+  ];
 
-    const locations = [
-        "Auckland",
-        "Wellington",
-        "Christchurch",
-        "Dunedin",
-        "Invercargill"
-    ];
-
-    return (
-        <Box display="flex" flexDirection="column" alignItems="center">
-            <label htmlFor="location" className={style["register-label"]}>Primary Location</label>
-            <select
-                name="location"
-                id={style["location"]}
-                className={style["register-input"]}
-                value={data.location}
-                onChange={handleChange}
-            >
-                <option value="" disabled>
-                    Which city should llama base the recommendations?
-                </option>
-                {locations.map((location) => (
-                    <option key={location} value={location}>
-                        {location}
-                    </option>
-                ))}
-            </select>
-        </Box>
-    );
-};
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <InputLabel
+        htmlFor="location"
+        className={style.registerLabel}
+        style={{
+          paddingBottom: "5px",
+          textTransform: "uppercase",
+          fontFamily:
+            "'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif",
+          color: "#fefefe",
+        }}
+      >
+        Primary Location
+      </InputLabel>
+      <Select
+        name="location"
+        id={style["location"]}
+        className={style["register-input"]}
+        value={data.location}
+        onChange={handleChange}
+        style={{
+          backgroundColor: "white",
+          borderRadius: "25px",
+          width: "71vh",
+          padding: "2vh",
+          height: "6vh",
+          color: "black", fontFamily: "Franklin Gothic Medium, 'Arial Narrow', Arial, sans-serif",
+          textAlign: "center",
+          fontWeight: "1px",
+          justifyContent: "center"
+        }}
+        disableUnderline={true}
+      >
+        <MenuItem value="" style={{color: "black"}} disabled>Which city should LLAMAFY base recommendations on?</MenuItem>
+        {locations.map((location) => (
+          <MenuItem key={location} value={location}>
+            {location}
+          </MenuItem>
+        ))}
+      </Select>
+    </Box>
+  );
+}
