15fa86684c54de0a73c26e548d1562d46d283e2e
kinzi ceolin <kceo161@aucklanduni.ac.nz>
2023-05-08 14:51:43 +1200

    remove unecessary console.log

--- frontend/src/components/WardrobeItems/WardrobeItems.jsx
+++ frontend/src/components/WardrobeItems/WardrobeItems.jsx
@@ -63,6 +63,7 @@ function WardrobeItems({ clothes, setClothes, category }) {
     <>
       {isItemsVisible &&
         clothes.map((item) => (
+          console.log("item", item),
           <List
             key={item.clothing_id}
             sx={{
@@ -89,6 +90,8 @@ function WardrobeItems({ clothes, setClothes, category }) {
                       variant="body2"
                       color="text.primary"
                     >
+                      {item.color} {item.sleeves} {item.pattern} {item.style}{" "}
+                      {item.sub_category}{" "}
                       <Button
                         onClick={() => handleDeleteItem(item)}
                         sx={{
@@ -100,8 +103,6 @@ function WardrobeItems({ clothes, setClothes, category }) {
                       >
                         <img src={bin} alt="bin button" width="15px" />
                       </Button>
-                      {item.color} {item.sleeves} {item.pattern}{" "}
-                      {item.sub_category}{" "}
                     </Typography>
                   </>
                 }
