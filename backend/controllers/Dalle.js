857a7569c2382c74c07ce0f3af3f48c27686c43c
kralkapoor <kkap897@aucklanduni.ac.nz>
2023-05-12 18:04:48 +1200

    Added a few comments to the dalle controller

--- backend/controllers/Dalle.js
+++ backend/controllers/Dalle.js
@@ -1,7 +1,4 @@
 require('dotenv').config();
-const axios = require('axios');
-const { v4: uuidv4 } = require('uuid');
-const fs = require('fs');
 const key = process.env.OPEN_AI_KEY;
 
 const { Configuration, OpenAIApi } = require("openai");
@@ -9,18 +6,18 @@ const configuration = new Configuration({ apiKey: key });
 const openai = new OpenAIApi(configuration);
 
 const imgGen = async (prompt) => {
-
+  
+  // This will call the DALL-E to generate a 512x512 image of a given prompt
+  // Each davinci return prompt contains three DALL-E generation prompts
+  // Therefore this is called three times per davinci response
   const response = await openai.createImage({
     prompt: prompt,
     n: 1,
     size: "512x512",
     response_format : "url",
-    // user : "Lllamas",
   });
 
-  console.log("Outfit Image Generated");
-
-  // Extract the URL from the response to save to the server
+  // Extract the URL from the response
   const genUrl = response.data.data[0].url;
   return genUrl;
 }
