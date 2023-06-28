cf4b983f04136af717dd062e913575bd19017498
Caitlin <109117308+CaitiGre@users.noreply.github.com>
2023-06-28 17:00:42 +1200

    Update database-config.js

--- backend/database/database-config.js
+++ backend/database/database-config.js
@@ -1,6 +1,5 @@
 module.exports = {
   connectionLimit: 100,
-  host: 'llamifydatabaseid.cvuxmmy1dyvs.ap-southeast-2.rds.amazonaws.com',
   user: 'admin',
   password: 'lavender',
   database: 'llamabase',
